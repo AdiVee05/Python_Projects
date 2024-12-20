9a99023675c4ec72805d4604543d4aee1ef7c698
Aditya Veeramony <aditya.dfw@gmail.com>
2024-12-20 17:11:00 -0600

    Create tau_challenge.py

---
+++ tau_challenge.py
@@ -0,0 +1,18 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Aditya Veeramony
+# Section:      467/567, 568, 474/574
+# Assignment:   Lab 3
+# Date:         1st of August, 2024
+
+import math
+
+# Value of Tau
+t = 6.283185307179586
+# Number of Digits
+x = int(input("Please enter the number of digits of precision for tau: "))
+
+# Prints according to number of digits 
+print(f"The value of tau to {x} digits is:{t: .{x}f}")
